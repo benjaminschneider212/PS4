1bd71b4e0aa1e91afaea64250d3dbb2ec54c7cb4
Benjamin Schneider <benjamin.schneider@wustl.edu>
2018-02-18 14:33:03 -0600

    Created Homework File
    Formatted the homework and put in the code from the homework to fix

---
+++ ProblemSet4.R
@@ -0,0 +1,30 @@
+############################
+###### Problem Set 4 #######
+############################
+#### Benjamin Schneider ####
+############################
+
+###GETTING STARTED###
+#Debug this code:
+myFunction<-function(doorthing, doorthing2, x){
+  doorthing1<-doorthing2<-sample(1:3, 1)
+  if (doorthing1==doorthing2){ x<-TRUE } else { x==FALSE }
+  x
+}
+myFunction(sample(1:3, 1), sample(1:3, 1))
+# Should return a TRUE if these samples are equal and
+# a false if they are not
+
+###MOVING ON###
+
+##Problem 1
+
+##Problem 2
+
+###SIMULATION###
+
+##Problem 1
+
+##Problem 2
+
+##Problem 3

211bb85fbf66c1787430cc750b66301e841cd730
Benjamin Schneider <benjamin.schneider@wustl.edu>
2018-02-19 12:10:54 -0600

    Debug 1
    Did the debug code

--- ProblemSet4.R
+++ ProblemSet4.R
@@ -12,12 +12,24 @@ myFunction<-function(doorthing, doorthing2, x){
   x
 }
 myFunction(sample(1:3, 1), sample(1:3, 1))
+debug(myFunction)
+
+myFunction<-function(doorthing1, doorthing2){
+  if (doorthing1==doorthing2){ x<-TRUE } else { x==FALSE }
+  return(x)
+}
+door1<-sample(1:3, 1)
+door2<-sample(1:3, 1)
+myFunction(door1, door2)
+
 # Should return a TRUE if these samples are equal and
 # a false if they are not
 
 ###MOVING ON###
 
 ##Problem 1
+
+
 
 ##Problem 2
 

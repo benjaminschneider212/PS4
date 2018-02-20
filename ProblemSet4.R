05751a4619cf4a92ebafa4de77bbfcea9c619fe6
Benjamin Schneider <benjamin.schneider@wustl.edu>
2018-02-19 18:37:27 -0600

    Work on validity test
    New additions

--- ProblemSet4.R
+++ ProblemSet4.R
@@ -29,7 +29,30 @@ myFunction(door1, door2)
 
 ##Problem 1
 
+chosenDoor<-sample(1:3,1)
+carDoor<-sample(1:3,1)
+switch<-c(TRUE,FALSE)
 
+setClass(Class="Door", #Here we set up the class 4 object
+         representation=representation(
+           chosenDoor = "integer",
+           carDoor="integer",
+           switch="logical"),
+         prototype=prototype(
+           chosenDoor=integer(),
+           carDoor=integer(),
+           switch=logical()))
+
+#now we need to do a validity test to make sure that we are only putting in values that make sense
+setValidity("Door", function(object){
+  chosenDoortest<-(is.integer(chosenDoor))
+  carDoortest<-is.integer(carDoor)
+  switchtest<-is.logical(switch)
+  if(!chosenDoortest || !carDoortest || !switchtest){return("Not a valid input")}
+} )
+
+door1<-new("Door", chosenDoor=3L, carDoor=3L, switch=TRUE) #This one works!
+door2<-new("Door", chosenDoor=3, carDoor=3, switch=4) #This causes all of the red alerts to go off.
 
 ##Problem 2
 
